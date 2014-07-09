0b40343768c55d7381cc1be5694dbf57ceef4525
Jeryn Mathew <jerynmathew@gmail.com>
2014-07-09 11:10:30 +0530

    Asset Manager
    	- Added urls for assets

--- AssetManager/apps/assets/serializers.py
+++ AssetManager/apps/assets/serializers.py
@@ -6,5 +6,5 @@ from .models import Assets
 class AssetSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Assets
-        fields = ('type', 'created_on', 'updated_on', 'attributes')
+        fields = ('type', 'created_on', 'updated_on', 'attributes', 'url')
 
